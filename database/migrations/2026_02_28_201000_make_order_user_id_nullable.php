64ec1ecbdb2b68635131a3c02b442d07e5516c14
samSignal <samndlovucrypto@gmail.com>
2026-02-28 21:18:01 +0200

    Make order user_id nullable to support guest orders in seeder

---
+++ database/migrations/2026_02_28_201000_make_order_user_id_nullable.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            // Make user_id nullable if it's not already
+            $table->foreignId('user_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            // We can't easily revert this without knowing if there are null values
+            // So we'll leave it as nullable
+        });
+    }
+};
