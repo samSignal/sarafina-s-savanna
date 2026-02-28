e7b855118970cb07abf3562f851544f2c0645aa0
samSignal <samndlovucrypto@gmail.com>
2026-02-28 14:45:56 +0200

    Automate database seeding for Render deployment

--- docker/run.sh
+++ docker/run.sh
@@ -5,5 +5,8 @@ cd /var/www/html
 # Run migrations
 php artisan migrate --force
 
+# Seed database (Automatically populate data on deployment)
+php artisan db:seed --force
+
 # Start Apache
 /usr/sbin/apache2ctl -D FOREGROUND
